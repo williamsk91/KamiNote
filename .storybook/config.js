432a018cc04f61914a6be8b0351c4870db356491
Williams <williamskusnandi@gmail.com>
2019-11-26 11:58:47 +1100

    improv: storybook defaults to not showing stories and addon panels

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,14 +3,19 @@ import { configure, addDecorator } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
 
 import { theme } from "../src/components/styles/theme";
+import { addParameters } from "@storybook/react";
 
-// add theme and padding
-addDecorator((storyFn, opts) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <div style={{ padding: "18px" }}>{storyFn()}</div>
-    </ThemeProvider>
-  );
+addParameters({
+  options: {
+    showAddonPanel: false,
+    showStoriesPanel: false
+  }
 });
+// add theme and padding
+addDecorator((storyFn, opts) => (
+  <ThemeProvider theme={theme}>
+    <div style={{ padding: "18px" }}>{storyFn()}</div>
+  </ThemeProvider>
+));
 
 configure(require.context("../src", true, /.stories.tsx?$/), module);
