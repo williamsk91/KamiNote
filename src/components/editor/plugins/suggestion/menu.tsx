a75cd2dd39705381660bb23c4569469492d82dba
williams <williamskusnandi@gmail.com>
2019-12-11 14:50:38 +1100

    fix: type imports

--- src/components/editor/plugins/suggestion/menu.tsx
+++ src/components/editor/plugins/suggestion/menu.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState, useEffect } from "react";
 import styled from "styled-components";
 
-import { IInlineSuggestion, ITextPos } from "./suggestion";
+import { IInlineSuggestion, ITextPos } from "./types";
 
 export interface ISuggestionTooltip {
   /**
