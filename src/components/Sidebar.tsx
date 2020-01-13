8c84b168884450a655abef6f502512e53c9bab89
williams <williamskusnandi@gmail.com>
2020-01-13 23:24:46 +1100

    style: Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from "react";
+import { FiPlusSquare, FiTrash2 } from "react-icons/fi";
 import { useHistory } from "react-router-dom";
-import { Icon, Menu } from "antd";
+import { Tree } from "antd";
 import styled from "styled-components";
 
 import { pageUrl } from "routes/pagePath";
+
+import { IconButton } from "./IconButton";
+import { colors } from "./styles/colors";
 
 interface IProp {
   pages: {
@@ -18,19 +22,56 @@ export const Sidebar: FC<IProp> = props => {
   const history = useHistory();
 
   return (
-    <Menu mode="inline" selectable={false}>
+    <Tree
+      onSelect={key => {
+        key[0] && history.push(pageUrl(key[0]));
+      }}
+    >
       {pages.map(p => (
-        <Item key={p.id} id={p.id} onClick={() => history.push(pageUrl(p.id))}>
-          <span>{p.title ? p.title : "Untitled"}</span>
-          <Icon type="plus-square" onClick={onAddPage} />
-        </Item>
+        <Tree.TreeNode
+          key={p.id}
+          title={
+            <TreeNodeContainer>
+              <NodeText>{p.title ? p.title : "Untitled"}</NodeText>
+              <NodeIcons>
+                <IconButton onClick={onAddPage}>
+                  <FiPlusSquare />
+                </IconButton>
+                <IconButton>
+                  <FiTrash2 />
+                </IconButton>
+              </NodeIcons>
+            </TreeNodeContainer>
+          }
+        />
       ))}
-    </Menu>
+    </Tree>
   );
 };
 
-const Item = styled(Menu.Item)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+const TreeNodeContainer = styled.div`
+  position: relative;
+`;
+
+const NodeText = styled.div`
+  display: block;
+  width: 158px;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const NodeIcons = styled.div`
+  position: absolute;
+  right: 0px;
+  top: 0px;
+
+  background: ${colors.white};
+  opacity: 0.9;
+  display: none;
+
+  ${TreeNodeContainer}:hover & {
+    display: block;
+  }
 `;
